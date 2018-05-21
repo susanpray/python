2410a78a8dabae76c09ac6e53b7c7fa8981d0620
susanpray <susanpray@hotmail.com>
2018-05-21 12:13:42 +0800

    Update loginlogout.py

--- loginlogout.py
+++ loginlogout.py
@@ -9,13 +9,13 @@ from optparse import OptionParser
 import logging
 
 if __name__ == '__main__':
-    uri = "http://zc.suning.com/"
+    uri = "http://xxxxxxxxxx/"
     method = 'chrome'
     webtest = webtester(uri,method)
     time.sleep(10)
     ## webtest.driver.find_element_by_link_text(u"登录").click()
-    ## webtest.driver.find_element_by_id('userName').send_keys('15295568019')
-    ## webtest.driver.find_element_by_id('password').send_keys('123qwe')
+    ## webtest.driver.find_element_by_id('userName').send_keys('xxxxx')
+    ## webtest.driver.find_element_by_id('password').send_keys('xxxxxxx')
     ## webtest.driver.find_element_by_id('submit').click()
     ## time.sleep(30)
     # webtest.driver.get(uri + "/project/browseList.htm")
